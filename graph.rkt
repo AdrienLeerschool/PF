6ee722f788f441450d59e4d025ca060289bacd9e
Adrien <adrien.leerschool@hotmail.com>
2020-04-18 10:05:54 +0200

    sf-add

--- graph.rkt
+++ graph.rkt
@@ -4,6 +4,7 @@
 ;-----------------------------------------------------------;
 
 (define pi (* 4 (atan 1 1)))
+(define ns (make-base-namespace))
 
 ;-----------------------------------------------------------;
 ;                      BASIC FUNCTIONS                      ;
@@ -33,7 +34,8 @@
 ; la liste 'var' et retourne une listes de NODEs dont les premiers éléments sont les NODEs de 'cst'
 ; suivies des NODEs de 'nodes' ajoutées à 'cst' (dans le même ordre que celui de la liste 'nodes' 
 ; initialement donnée) et dont les derniers éléments sont les NODEs de la liste 'var' suivies des NODEs
-; de 'nodes' ajoutées à 'var' (dans le même ordre que celui de la liste 'nodes' initialement donnée)
+; de 'nodes' ajoutées à 'var' (dans le même ordre que celui de la liste 'nodes' initialement donnée).
+; First call : (re-order nodes '() '())
 (define re-order
   (lambda (nodes cst var)
     (if (null? nodes) (append (reverse cst) (reverse var))
@@ -47,7 +49,7 @@
 ; présentes dans la table 't' ont été remplacées dans le graphe par leur valeur correspondante.
 (define replace-fn
   (lambda (tri t)
-     (map (replace t) (cadr tri))))
+     (re-order (map (replace t) (cadr tri)) '() '())))
                     
 
 ; Prend en entrée un NODE 'node' et une table d'assignation 't' et retourne un NODE dont le STORE
@@ -69,32 +71,6 @@
           (cond ((null? nd_arg) store)                                        ; we want the store value
                 ((list? (car nd_arg)) ((replace (car nd_arg)) (node store)))  ; we want to replace variables by their valuesµ
                 (else ((car nd_arg) store)))))))                              ; we want to apply a function to the node
-
-
-;-----------------------------------------------------------;
-;                           TESTS                           ;
-;-----------------------------------------------------------;
-
-
-(define recover
-  (lambda (nd)
-    (let ((val nd))
-      (val))))
-
-(define inc
-   (lambda (a)
-     (+ a 1)))
-
-((node 2) inc)     ;[[(node f)]]
-(inc ((node 2)))   ;[[(f store)]]
-((node 'x) '((y . 1) (x . 4)))
-
-(define triplet (cons '+ (cons '((node 1) (node 2) (node 3)) (list inc))))
-(define tripl (cons '+ (cons '((node 1) (node 2) (node 3)) (list (caddr triplet)))))
-((caddr triplet) 4)
-((caddr tripl) 4)
-
-(equal? ((node 'x)) ((node 'x)))
 
 
 
@@ -139,9 +115,9 @@
 ; soit un VAR_STORE), 'sf-aux' retourne ce même NODE. Si le STORE est un
 ; FN_STORE, retourne ce NODE simplifié si c'est possible.
 (define sf-aux
-  (lambda (node)
-    (let ((store (node)))
-      (if (atom? store) node                ; si le STORE est un CST_STORE ou un VAR_STORE
+  (lambda (nd)
+    (let ((store (nd)))
+      (if (atom? store) nd                  ; si le STORE est un CST_STORE ou un VAR_STORE
           ((caddr store) (cadr store))))))  ; si le STORE est un FN_STORE
 
 
@@ -149,30 +125,57 @@
 ; et retourne 1 si les 2 arguments sont égaux (dans le sens de 'equal?'). 0 aussi non.
 (define is-eq?
   (lambda (val)
-    (lambda (node)
-      (if (equal? val (node)) #t #f))))
+    (lambda (nd)
+      (if (equal? val (nd)) #t #f))))
 
 ; Prend en entrée un NODE et une valeur booléenne 'bool' et retourne une liste contenant uniquement
 ; le NODE si 'bool' est vrai, retourne une liste vide aussi non.
 (define remove
-  (lambda (node bool)
-    (if bool '() (list node))))
+  (lambda (nd bool)
+    (if bool '() (list nd))))
 
-; Prends en entrée une liste de NODEs 'nodes' et retourne une liste de NODEs dont le premier élément
-; est la somme de toutes les NODEs de 'nodes' dont le STORE est un CST_STORE et ou les autres NODEs
-; gardent leur ordre d'avant la simplification.
+; Prends en entrée une liste de NODEs 'nodes' et un compteur 'cnt' (initialisé à 0 au premier appel)
+; et retourne une liste de NODEs dont le premier élément est la somme de toutes les NODEs de 'nodes'
+; dont le STORE est un CST_STORE + cnt et ou les autres NODEs gardent leur ordre d'avant la simplification. 
+; Si la somme des constantes vaut zero et qu'il reste d'autres NODEs dans la liste, supprime le NODE
+; contenant le CST_STORE nul, retourne une node dont le STORE vaut 0 aussi non.
 (define add-cst
-          
+  (lambda (args cnt)
+    (if (null? args) (list (node cnt))
+        (let ((nxt (car args)))
+          (if (number? (nxt)) (add-cst (cdr args) (+ cnt (nxt)))
+              (if (= 0 cnt) args
+                  (cons (node cnt) args)))))))
+    
+        
+             
 (define sf-add
-  (lambda args
-    (let ((nodes1 (map sf-aux args)))
-      (let ((nodes2 (add-cst nodes1)))                                         ; add the constants together
-        (let ((nodes3 (append (map remove nodes2 (map (is-eq? 0) nodes2)))))   ; ('+ ((node a) (node 0)) sf-add) => ('+ node a)
-          (let 
-              ; ('+ ((node a) ('+ ((node b) (node c)) sf-add) sf-add) => ('+ ((node a) (node b) (node c)) sf-add)
-              ; check if + 2 constantes qu'on peut regrouper
+  (lambda (args)
+    (let ((nodes (add-cst (map sf-aux args) 0)))
+      (if (= 1 (length nodes)) (car nodes)
+          nodes))))
         
         
 (define sf-mult
   (lambda (args)
     (args)))
+
+;-----------------------------------------------------------;
+;                         AFFICHAGE                         ;
+;-----------------------------------------------------------;
+;-----------------------------------------------------------;
+;                           TESTS                           ;
+;-----------------------------------------------------------;
+
+(define disp
+  (lambda (nodes vals)
+    (if (not (list? nodes)) (nodes)
+        (if (null? nodes) (reverse vals)
+            (disp (cdr nodes) (cons ((car nodes)) vals))))))
+
+
+(define ls (list '+ (list (node (list '+ (list (node 'x) (node 'y)) sf-add)) (node 'z)) sf-add))
+(define nodes (node ls))
+(disp nodes '())
+(nodes)
+
